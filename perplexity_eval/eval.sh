bbc90b6ff7d5a82c35bb82820c3c58dce007e7d9
ArminAzizi98 <147081650+ArminAzizi98@users.noreply.github.com>
2024-09-09 10:28:27 -0700

    Create eval.sh

---
+++ perplexity_eval/eval.sh
@@ -0,0 +1,13 @@
+python -u eval.py \
+    --tokenized PY007/tokenized_proof_pile_test_neox \
+    --dataset-min-tokens 32768 \
+    --samples 20 \
+    --split test \
+    --output-file figure \
+    --min-tokens 32768 \
+    --max-tokens 32768 \
+    --tokens-step 32768 \
+    --truncate \
+    --delta_ratio 1.0 \
+    -m state-spaces/mamba-1.4b
+python plot.py --xmax 12288 --ymax 20 figure
