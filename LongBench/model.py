490e0adb151876e2e3d65a9f5068b6c1b22677dc
ArminAzizi98 <147081650+ArminAzizi98@users.noreply.github.com>
2025-04-08 12:05:45 -0700

    Update model.py

--- LongBench/model.py
+++ LongBench/model.py
@@ -1,8 +1,3 @@
 # Load model directly
-from huggingface_hub import login
-login('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')
-from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# Load model directly
-from transformers import AutoModel
-model = AutoModel.from_pretrained("state-spaces/mamba2-2.7b")
+from transformers import AutoTokenizer, AutoModelForCausalLM
